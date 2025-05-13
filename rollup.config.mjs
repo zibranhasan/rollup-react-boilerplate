ddf3a1a4239de2a38d5858c1801620e9d8ef09a9
zibranhasan <zibranhasansourab@gmail.com>
2025-05-13 13:55:05 +0600

    all set

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import { babel } from "@rollup/plugin-babel";
+
+// rollup.config.mjs
+export default {
+  input: "src/main.js",
+  output: {
+    file: "public/bundle.js",
+    format: "esm",
+  },
+  plugins: [
+    nodeResolve({ extensions: [".js", ".jsx"] }),
+
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+      extensions: [".js", ".jsx"],
+    }),
+    commonjs(),
+    replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+  ],
+};
---
+++ src/another.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const HelloWorld = () => {
+  return React.createElement("p", {}, "Hello world from another file");
+};
